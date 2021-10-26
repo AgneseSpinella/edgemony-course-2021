e0a76351b856e5f574e18a7c9c71f74851320307
Agnese <92723905+AgneseSpinella@users.noreply.github.com>
2021-10-26 18:49:11 +0200

    Esercitazione svolta

--- 26Oct/esercitazione.js
+++ 26Oct/esercitazione.js
@@ -18,3 +18,62 @@
     SUGGERIMENTO: esistono delle funzioni della libreria Math (vista a lezione).
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
+
+const sum = (numOne, numTwo) => numOne + numTwo;
+const subt = (numOne, numTwo) => numOne - numTwo;
+const mult = (numOne, numTwo) => numOne * numTwo;
+const divis = (numOne, numTwo) => numOne / numTwo;
+const pow = (numOne, numTwo) => numOne ** numTwo;
+const rad = (numOne) => Math.sqrt(numOne);
+const pattern = new RegExp(/^\D/)
+
+function calculator(operation, numbers) {
+    let total= 0;
+    total = operation(...numbers);
+    return total;
+}
+
+let chosenOperation = prompt ("Scegli l'operazione che vuoi effettuare:\n -somma\n -sottrazione\n -moltiplicazione\n -divisione\n -Potenza\n -Radice quadrata")
+let chosenNumbers = prompt ("Scrivi i due numeri (separati da una virgola) tra cui vuoi effettuare l'operazione. Se hai scelto la radice quadrata, scrivi un solo numero")
+const numbers= chosenNumbers.split(',');
+const array = [];
+
+for (n of numbers) {
+    if (pattern.test(n)) {
+        alert("Hai inserito un carattere non numerico")
+        break
+    } else {
+        array.push(parseInt(n));
+    }
+}
+
+//possibili errori
+if (array.length == 1 && chosenOperation !== "radice") {
+    console.log(array[0]);
+} else if (array.length > 2) {
+    alert("Hai inserito troppi numeri. L'operazione verrà eseguita soltanto tra i primi due numeri")
+}
+    
+
+switch (chosenOperation.toLowerCase()) {
+    case "somma":
+        calculator(sum, array);
+        break;
+    case "moltiplicazione":
+        calculator(mult, array);
+        break;
+    case "sottrazione":
+        calculator(subt, array);
+        break;
+    case "divisone":
+        calculator(divis, array);
+        break;
+    case "potenza":
+        calculator(pow, array);
+        break;
+    case "radice quadrata":
+        calculator(rad, array);
+        break
+    default:
+        alert("Non hai inserito i valori correttamente. Riprova!")
+}
